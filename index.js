26a679bc092915f5ea98cb07869068a9f41453f5
Maria Marquez <406rem33@gmail.com>
2022-03-20 10:59:42 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const names =["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, eventName) {
+    const bnk = [];
+    for (let i = 0; i < names.length; i++) {
+        bnk.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+  }
+
+  return bnk;
+}
+
+writeCards(cards);
+
+
+
+function countDown(bob){
+ while (bob >= 0) {
+     console.log(bob--);
+};
+};
